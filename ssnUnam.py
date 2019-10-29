47ac8363f0f192185fc94e874035419a542d4796
YonatanRA <yeho@mail.com>
2019-10-28 18:35:50 -0600

    ssnUnam

---
+++ ssnUnam.py
@@ -0,0 +1,65 @@
+import traceback
+import urllib3
+import xmltodict
+import re
+import pymongo 
+import time
+
+
+
+client=pymongo.MongoClient()
+db=client.earthquake
+
+
+
+
+def getxml():
+    url = 'http://www.ssn.unam.mx/rss/ultimos-sismos.xml'
+
+    http = urllib3.PoolManager()
+
+    response = http.request('GET', url)
+    
+    try:
+        data = xmltodict.parse(response.data)
+    except:
+        print('Failed to parse xml from response {}'.format(traceback.format_exc()))
+    
+    return data
+    
+    
+    
+    
+def earth():
+	
+	d=getxml()
+	
+	for i in range(len(d['rss']['channel']['item'])):
+		
+		data_get=d['rss']['channel']['item'][i]['description'].split('>')[1:4]
+		
+		fecha=re.findall('\d+-\d+-\d+', data_get[0])[0]
+		hora=re.findall('\d+:\d+:\d+', data_get[0])[0]
+		lat=re.findall('-?\d+.\d+', data_get[1])[0]
+		lon=re.findall('-?\d+.\d+', data_get[1])[1]
+		prof=re.findall('\d+.\d+ \w+', data_get[2])[0]
+		
+		inten=float(d['rss']['channel']['item'][i]['title'].split(',')[0])
+		
+		data={'Fecha':fecha, 'Hora':hora,
+			  'Latitud':float(lat), 'Longitud':float(lon),
+			  'Profundidad':prof, 'Intensidad':inten}
+		
+		db.ssnUnam.update(data, data, upsert=True)
+
+
+
+
+
+while 1:
+	earth()
+	print ('paso1')
+	print ()
+	time.sleep(10)
+	print ('paso2')
+	print ()
